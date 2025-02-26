edc75985fa286afeaa2156ef0930dbdfe887f08c
dessinfleurss <anabelhuanaco@gmail.com>
2025-02-26 10:17:21 -0300

    Enlace de las direcciones de los departamentos

--- flaskr/departments.py
+++ flaskr/departments.py
@@ -11,9 +11,20 @@ bp = Blueprint('departments', __name__, url_prefix='/departments')
 def index():
     db = get_db()
     departments = db.execute(
-        """SELECT department_name AS departamentos, city AS ciudad, state_province AS provincia, country_name AS pais FROM departments depa
+        """SELECT department_name AS nombre, city AS ciudad, state_province AS provincia, country_name AS pais, department_id AS id FROM departments depa
         JOIN locations loca ON depa.location_id = loca.location_id
         JOIN countries p ON loca.country_id = p.country_id
-        ORDER BY departamentos"""
+        ORDER BY nombre"""
     ).fetchall()
     return render_template('depas/index.html', departments=departments)
+
+@bp.route('/<int:id>/detalle')
+def detalle(id):
+    db = get_db()
+    d = db.execute(
+        """SELECT department_name AS nombre, city AS ciudad, state_province AS provincia, country_name AS pais FROM departments depa
+        JOIN locations loca ON depa.location_id = loca.location_id
+        JOIN countries p ON loca.country_id = p.country_id
+        WHERE department_id = ?""", (id,)
+    ).fetchone()
+    return render_template('depas/detalle.html', d=d)
